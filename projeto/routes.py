a16b96f9d055a68afca69e33032c1b04c7f8eb17
ThalitaAprogramadora <thatamatheus29@gmail.com>
2023-06-01 15:06:41 +0100

    thalita

--- projeto/routes.py
+++ projeto/routes.py
@@ -1,2 +1,7 @@
+from flask import Flask, render_template
 from projeto import app
-from flask import flask, render_template
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
