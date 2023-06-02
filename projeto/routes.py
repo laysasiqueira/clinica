9bb37e1d38a7fb773fcde264bb5a0280caae43ad
Dikshab06 <135232799+Dikshab06@users.noreply.github.com>
2023-06-02 09:08:40 +0100

    diksha

--- projeto/routes.py
+++ projeto/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from projeto import app
 
-
+app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
@@ -10,6 +10,10 @@ def index():
 def login():
     return render_template('login.html')
 
+
 @app.route('/registro')
 def registro():
     return render_template('registro.html')
+
+if __name__ == '__main__':
+    app.run()
